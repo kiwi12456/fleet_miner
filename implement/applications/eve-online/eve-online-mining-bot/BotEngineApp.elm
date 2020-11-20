e7288a09bc512f6915e014d35304503ddd2d7a05
Charles Lee <charlesjhlee@gmail.com>
2020-11-21 03:31:08 +1100

    test

--- implement/applications/eve-online/eve-online-mining-bot/BotEngineApp.elm
+++ implement/applications/eve-online/eve-online-mining-bot/BotEngineApp.elm
@@ -248,7 +248,7 @@ returnDronesAndRunAwayIfHitpointsAreTooLow context shipUI =
         runAwayWithDescription =
             describeBranch
                 ("Shield hitpoints are at " ++ (shipUI.hitpointsPercent.shield |> String.fromInt) ++ "%. Run away.")
-                (runAway context)
+                (dockToUnloadOre context)
     in
     if shipUI.hitpointsPercent.shield < context.eventContext.appSettings.runAwayShieldHitpointsThresholdPercent then
         Just runAwayWithDescription
