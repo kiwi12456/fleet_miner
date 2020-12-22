daec1ff863134593531559be0643d7341e7e0d07
Charles Lee <charlesjhlee@gmail.com>
2020-12-23 04:13:14 +1100

    t

--- implement/applications/eve-online/eve-online-mining-bot/BotEngineApp.elm
+++ implement/applications/eve-online/eve-online-mining-bot/BotEngineApp.elm
@@ -71,7 +71,7 @@ defaultBotSettings =
     , targetingRange = 8000
     , miningModuleRange = 5000
     , botStepDelayMilliseconds = 4000
-    , oreHoldMaxPercent = 95
+    , oreHoldMaxPercent = 30
     , selectInstancePilotName = Nothing
     }
 
