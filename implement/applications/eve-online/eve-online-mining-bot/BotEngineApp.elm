1bc2a0ffdc752ff61457b94dfeb98d436ca2db7d
Charles Lee <charlesjhlee@gmail.com>
2020-12-28 01:26:08 +1100

    t

--- implement/applications/eve-online/eve-online-mining-bot/BotEngineApp.elm
+++ implement/applications/eve-online/eve-online-mining-bot/BotEngineApp.elm
@@ -1333,7 +1333,7 @@ overviewWindowEntryRepresentsNoFill : OverviewWindowEntry -> Bool
 overviewWindowEntryRepresentsNoFill entry =
     entry.uiNode
         |> listDescendantsWithDisplayRegion
-        |> List.any (.uiNode >> .pythonObjectTypeName >> String.toLower >> String.contains "fill") |> not
+        |> List.any (.pythonObjectTypeName >> String.toLower >> String.contains "fill") |> not
 
 overviewWindowEntryRepresentsAnAsteroid : OverviewWindowEntry -> Bool
 overviewWindowEntryRepresentsAnAsteroid entry =
